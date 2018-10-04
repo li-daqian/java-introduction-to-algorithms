86f2d454e8df4d1e45b862d664cc899fd046c82d
aq0706 <195853932@qq.com>
2018-10-04 22:59:46 +0800

    Patch

--- src/main/java/dynamicprogramming/OptimalSubstructure.java
+++ src/main/java/dynamicprogramming/OptimalSubstructure.java
@@ -6,6 +6,12 @@ package dynamicprogramming;
  */
 public class OptimalSubstructure {
 
+    /**
+     * 钢条切割-自顶向下递归实现
+     * @param array 钢筋长度价格表
+     * @param length 需要切割的钢筋长度
+     * @return 钢筋最大价格
+     */
     public static int cutRod(int[] array, int length) {
         if (length == 0) {
             return 0;
@@ -17,5 +23,94 @@ public class OptimalSubstructure {
         }
 
         return result;
+    }
+
+    /**
+     * 钢条切割-动态规划 自顶向下带备忘实现
+     * @param array 钢筋长度价格表
+     * @param length 需要切割的钢筋长度
+     * @return 钢筋最大价格
+     */
+    public static int memoizedCutRod(int[] array, int length) {
+        int[] answer = new int[length + 1];
+        for (int i = 1; i < answer.length; i++) {
+            answer[i] = Integer.MIN_VALUE;
+        }
+
+        return memoizedCutRodAux(array, length, answer);
+    }
+
+    /**
+     * 钢条切割-动态规划 自底向上
+     * @param array 钢筋长度价格表
+     * @param length 需要切割的钢筋长度
+     * @return 钢筋最大价格
+     */
+    public static int bottomUpCutRod(int[] array, int length) {
+        int[] answer = new int[length + 1];
+        answer[0] = 0;
+
+        for (int i = 1; i <= length; i++) {
+            int result = Integer.MIN_VALUE;
+            for (int j = 1; j <= i; j++) {
+                result = Math.max(result, array[j] + answer[i - j]);
+            }
+            answer[i] = result;
+        }
+
+        return answer[length];
+    }
+
+    public static void printCutRodSolution(int[] array, int length) {
+        BestSolution bestSolution = extendedBottomUpCutRod(array, length);
+        while (length > 0) {
+            System.out.println(bestSolution.s[length]);
+            length = length - bestSolution.s[length];
+        }
+    }
+
+    private static BestSolution extendedBottomUpCutRod(int[] array, int length) {
+        BestSolution bestSolve = new BestSolution();
+        int[] r = new int[length + 1];
+        int[] s = new int[length + 1];
+        bestSolve.r = r;
+        bestSolve.s = s;
+        r[0] = 0;
+
+        for (int i = 1; i <= length; i++) {
+            int q = Integer.MIN_VALUE;
+            for (int j = 1; j <= i; j++) {
+                if (q < (array[j] + r[i - j])) {
+                    q = array[j] + r[i - j];
+                    s[i] = j;
+                }
+            }
+            r[i] = q;
+        }
+        return bestSolve;
+    }
+
+    private static int memoizedCutRodAux(int[] array, int length, int[] answer) {
+        if (answer[length] >= 0) {
+            return answer[length];
+        }
+
+        int q = 0;
+        if (length == 0) {
+            q = 0;
+        } else {
+            q = Integer.MIN_VALUE;
+            for (int i = 1; i <= length; i++) {
+                q = Math.max(q, array[i] + memoizedCutRodAux(array, length - i, answer));
+            }
+        }
+        answer[length] = q;
+
+        return q;
+    }
+
+    static class BestSolution {
+        int[] r;
+        int[] s;
     }
 }
--- src/test/java/dynamicprogramming/OptimalSubstructureTest.java
+++ src/test/java/dynamicprogramming/OptimalSubstructureTest.java
@@ -1,12 +1,26 @@
 package dynamicprogramming;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * @author LiDaQian
  */
 public class OptimalSubstructureTest {
+
+    private OutputStream outContent = new ByteArrayOutputStream();
+    private PrintStream originStream = System.out;
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     public void testCutRod() {
@@ -22,5 +36,51 @@ public class OptimalSubstructureTest {
         Assert.assertEquals(22, OptimalSubstructure.cutRod(prices, 8));
         Assert.assertEquals(25, OptimalSubstructure.cutRod(prices, 9));
         Assert.assertEquals(30, OptimalSubstructure.cutRod(prices, 10));
+    }
+
+    @Test
+    public void testMemoizedCutRod() {
+        int[] prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+        Assert.assertEquals(1, OptimalSubstructure.memoizedCutRod(prices, 1));
+        Assert.assertEquals(5, OptimalSubstructure.memoizedCutRod(prices, 2));
+        Assert.assertEquals(8, OptimalSubstructure.memoizedCutRod(prices, 3));
+        Assert.assertEquals(10, OptimalSubstructure.memoizedCutRod(prices, 4));
+        Assert.assertEquals(13, OptimalSubstructure.memoizedCutRod(prices, 5));
+        Assert.assertEquals(17, OptimalSubstructure.memoizedCutRod(prices, 6));
+        Assert.assertEquals(18, OptimalSubstructure.memoizedCutRod(prices, 7));
+        Assert.assertEquals(22, OptimalSubstructure.memoizedCutRod(prices, 8));
+        Assert.assertEquals(25, OptimalSubstructure.memoizedCutRod(prices, 9));
+        Assert.assertEquals(30, OptimalSubstructure.memoizedCutRod(prices, 10));
+    }
+
+    @Test
+    public void testBottomUpCutRod() {
+        int[] prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+        Assert.assertEquals(1, OptimalSubstructure.bottomUpCutRod(prices, 1));
+        Assert.assertEquals(5, OptimalSubstructure.bottomUpCutRod(prices, 2));
+        Assert.assertEquals(8, OptimalSubstructure.bottomUpCutRod(prices, 3));
+        Assert.assertEquals(10, OptimalSubstructure.bottomUpCutRod(prices, 4));
+        Assert.assertEquals(13, OptimalSubstructure.bottomUpCutRod(prices, 5));
+        Assert.assertEquals(17, OptimalSubstructure.bottomUpCutRod(prices, 6));
+        Assert.assertEquals(18, OptimalSubstructure.bottomUpCutRod(prices, 7));
+        Assert.assertEquals(22, OptimalSubstructure.bottomUpCutRod(prices, 8));
+        Assert.assertEquals(25, OptimalSubstructure.bottomUpCutRod(prices, 9));
+        Assert.assertEquals(30, OptimalSubstructure.bottomUpCutRod(prices, 10));
+    }
+
+    @Test
+    public void testPrintCutRodSolution() {
+        int[] prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+        OptimalSubstructure.printCutRodSolution(prices, 7);
+
+        Assert.assertEquals("1\n6\n", outContent.toString());
+    }
+
+    @After
+    public void clean() {
+        System.setOut(originStream);
     }
 }
